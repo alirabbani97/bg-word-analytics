1b6738172aee060c050edcbe6d4135d8562c9d8a
Ali <alirabbani54@gmail.com>
2024-08-05 03:41:00 +0500

    update

--- src/TextArea.jsx
+++ src/TextArea.jsx
@@ -1,5 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
+import Warning from "./Warning";
 
 export default function TextArea() {
-  return <textarea className="textarea" />;
+  const [text, setText] = useState("");
+  const [errors, setErrors] = useState("");
+
+  const handleChange = (e) => {
+    let newText = e.target.value;
+    if (newText.includes("<script>")) {
+      newText = newText.replace("<script>", "");
+      setErrors("No Script Tag allowed");
+    } else if (newText.includes("@")) {
+      newText = newText.replace("@", "");
+      setErrors("No @ character allowed");
+    } else {
+      setErrors("");
+    }
+
+    setText(newText);
+  };
+
+  return (
+    <div className="textarea">
+      <textarea
+        value={text}
+        onChange={(e) => handleChange(e)}
+        placeholder="Enter Your Text"
+        spellCheck={false}
+      />
+      {errors && <Warning message={errors} />}
+    </div>
+  );
 }
