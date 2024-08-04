5a4d723f9a67ff8232d95168900d7e74b7b9bca6
Ali <alirabbani54@gmail.com>
2024-08-05 02:39:20 +0500

    init

---
+++ src/Container.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import TextArea from "./TextArea";
+import Stats from "./Stats";
+
+export default function Container() {
+  return (
+    <main className="container">
+      <TextArea />
+      <Stats />
+    </main>
+  );
+}
---
+++ src/Footer.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Footer() {
+  return <footer className="footer"><small>&copy; Copyrights All rights reserved aliparttwo.</small></footer>;
+}
